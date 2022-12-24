bb9074a2abeb7ebc64e865ff7346e2bc4ceffe55
Ozkan <ozkan.okcesiz@gmail.com>
2022-12-25 01:32:41 +0300

    add change index

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const cors = require("cors");
+const colors = require("colors");
 
-
-const express = require('express');
-require('dotenv').config();
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema');
-const cors = require('cors');
-const colors = require('colors');
-
-const connectDB = require('./config/db');
-
-
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
 
@@ -19,9 +15,17 @@ connectDB();
 
 app.use(cors());
 
-app.use('/graphql',graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+app.get("/", (req, res) => res.send("Hello word"));
 
-app.listen(port, console.log(`Server is running on http://localhost:${port}/graphql`.blue));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(
+  port,
+  console.log(`Server is running on http://localhost:${port}/graphql`.blue)
+);
